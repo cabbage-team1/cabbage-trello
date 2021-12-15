9e269c74740269eebc2c210e1ca23dadeb4af88a
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-15 17:50:50 +0800

    [yangyang] test: alter method

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -3,18 +3,14 @@ var t = window.TrelloPowerUp.iframe();
 onRecordBtnClick = function () {
     var changingTimes = t.get('card', 'shared', 'changeTime');
     if (changingTimes) {
-        changingTimes.then(function (board) {
-            changingTimes = changingTimes + 1;
-            console.log(JSON.stringify(board, null, 2));
-            t.set('card', 'shared', {"changeTime": changingTimes}).then(function (savedTimes) {
-                console.log(JSON.stringify(savedTimes, null, 2))
-            })
+        changingTimes.then(function (recordTime) {
+            recordTime = recordTime + 1;
+            console.log(JSON.stringify(recordTime, null, 2));
+            t.set('card', 'shared', {"changeTime": recordTime});
         })
     } else {
         changingTimes = 0;
-        t.set('card', 'shared', {"changeTime": changingTimes}).then(function (savedTimes) {
-            console.log(JSON.stringify(savedTimes, null, 2))
-        })
+        t.set('card', 'shared', {"changeTime": changingTimes});
     }
 }
 
