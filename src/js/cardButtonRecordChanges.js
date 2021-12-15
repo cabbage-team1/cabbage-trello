48f8e352c550212c5eaa5c6e8e35c0afa7afca9b
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-15 17:36:03 +0800

    [yangyang] test: add data

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -1,16 +1,11 @@
 var t = window.TrelloPowerUp.iframe();
 
-let changingTimes = 0;
 onRecordBtnClick = function (){
-    console.log("clickRecord =====" + changingTimes);
-    t.get('board','shared','changeTime').then(function (board) {
+    let changingTimes = t.get('card','shared','changeTime').then(function (board) {
         console.log(JSON.stringify(board,null,2));
     });
-    t.get('board','shared').then(function (boardAll) {
-        console.log(JSON.stringify(boardAll,null,2));
-    });
     changingTimes = changingTimes + 1;
-    t.set('board','shared',{"changeTime":changingTimes}).then(function (savedTimes){
+    t.set('card','shared',{"changeTime":changingTimes}).then(function (savedTimes){
         console.log(JSON.stringify(savedTimes,null,2))
     })
 }
