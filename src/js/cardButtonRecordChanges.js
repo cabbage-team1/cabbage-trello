c66cd4c93533b7ab37b377d95cb72d5d02619163
yulin <yulin.chen@thoughtworks.com>
2021-12-16 14:24:46 +0800

    [yulin] feat: use const `REQUIREMENT_CHANGE_TEXT` to store the template string

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -1,38 +1,29 @@
 const t = window.TrelloPowerUp.iframe();
 
 const context = t.getContext();
-console.log("context=",context);
+const REQUIREMENT_CHANGE_TEXT = `Total Changes: ${requirementChangeCount}`;
 let requirementChangeCount;
 t.get(context.card, 'shared', 'requirementChangeCount', 0).then(requirementChangeCountInResponse => {
     requirementChangeCount = requirementChangeCountInResponse;
-    showRequirementChangeCount(`total changes: ${requirementChangeCount}`);
+    showRequirementChangeCount(REQUIREMENT_CHANGE_TEXT);
 });
-t.cards("all").then(function (cards) {
-    console.log('t.cards(\'all\') res: ', JSON.stringify(cards, null, 2));
-});
-
 
 onRecordBtnClick = () => {
     requirementChangeCount = requirementChangeCount + 1;
-    console.log("requirementChangeCount is increased, now its value is: ", requirementChangeCount);
-    showRequirementChangeCount(`total changes: ${requirementChangeCount}`);
+    showRequirementChangeCount(REQUIREMENT_CHANGE_TEXT);
 }
 
 onSaveBtnClick = () => {
     t.set(context.card, 'shared', {requirementChangeCount})
-        .then(res => {
-                console.log("requirementChangeCount is saved!");
-                console.log('the response is: ', res);
-                showRequirementChangeCount(`total changes: ${requirementChangeCount} (save successfully!)`);
-        },
-            error => {
-                console.log("requirementChangeCount is saved!");
-                console.log('the error response is: ', error);
-                showRequirementChangeCount(`total changes: ${requirementChangeCount} (failed to save!)`);
+        .then(() => {
+                showRequirementChangeCount(REQUIREMENT_CHANGE_TEXT + '(save successfully!)');
+            },
+            () => {
+                showRequirementChangeCount(REQUIREMENT_CHANGE_TEXT + '(failed to save!)');
             });
 }
 
 showRequirementChangeCount = requirementChangeCount => {
-    let element = document.getElementById("requirementChangeCount");
+    let element = document.getElementById('requirementChangeCount');
     element.innerHTML = requirementChangeCount;
 }
