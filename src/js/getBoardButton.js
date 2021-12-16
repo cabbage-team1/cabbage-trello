d7e9de2ab8db346dd12cc275a13ed06b6599153e
pengfei <pengfei.qian@thoughtworks.com>
2021-12-16 13:12:41 +0800

    [pengfei] feat :#25 add board button 4

--- src/js/getBoardButton.js
+++ src/js/getBoardButton.js
@@ -2,9 +2,11 @@ export function getBoardButton (t, opts) {
     return [{
         text:'Requirement Changes',
         condition:'always',
-        callback:t.modal({
-            title:'Requirement Changes Analysis',
-            fullscreen:true,
-        })
+        callback:function(t, opt){
+            t.modal({
+                title:'Requirement Changes Analysis',
+                fullscreen:true,
+            })
+        }
     }];
 }
