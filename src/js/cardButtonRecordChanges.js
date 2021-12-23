fab3c6b9e3b4425f404fe23ffa246825da41b547
jinxin26 <xin.jin@thoughtworks.com>
2021-12-23 16:07:12 +0800

    [jinxin] feat: #36 save description to database and update version info when save button clicked

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const t = window.TrelloPowerUp.iframe();
 
 const context = t.getContext();
@@ -7,12 +8,68 @@ t.get(context.card, 'shared', 'requirementChangeCount',0).then(requirementChange
     showRequirementChangeCount(`Total Changes: ${requirementChangeCount}`);
 });
 
-onRecordBtnClick = () => {
+let info = {
+    cardId: '',
+    descriptions: '',
+    version: ''
+}
+
+const addBtnForVersionRecord = (list, versionRecord, curPage) => {
+    versionRecord.remove();
+    versionRecord = document.createElement("div");
+    document.body.appendChild(versionRecord);
+    versionRecord.id = "versionRecord";
+
+    for (let i = list.data.length - 1 - curPage * 5; i >= list.data.length - curPage * 5 - 5 && i >= 0; i--) {
+        const button = document.createElement("button");
+        button.textContent = list.data[i].version;
+        button.addEventListener('click', function () {onVersionBtnCLick(button.textContent)});
+        versionRecord.appendChild(button);
+    }
+
+    if(list.data.length > 5 || curPage !== 0) {
+        const prevPage = document.createElement("button");
+        prevPage.textContent = "<";
+        prevPage.onclick = function() {
+            if(curPage > 0) {
+                curPage = curPage - 1;
+                addBtnForVersionRecord(list, versionRecord, curPage);
+            }
+        }
+        versionRecord.appendChild(prevPage);
+
+        const nextPage = document.createElement("button");
+        nextPage.textContent = ">";
+        nextPage.onclick = function() {
+            if(curPage <= list.data.length / 5)
+            {
+                curPage = curPage + 1;
+                addBtnForVersionRecord(list, versionRecord, curPage);
+            }
+        }
+        versionRecord.appendChild(nextPage);
+    }
+}
+
+function onVersionBtnCLick(text) {
+    console.log('text', text);
+}
+
+const getVersionRecord = () => {
+    axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+        const versionRecord = document.getElementById("versionRecord");
+        let curPage = 0;
+        addBtnForVersionRecord(list, versionRecord, curPage);
+    });
+}
+getVersionRecord();
+
+window.onRecordBtnClick = function onRecordBtnClick() {
     requirementChangeCount = requirementChangeCount + 1;
     showRequirementChangeCount(`Total Changes: ${requirementChangeCount}`);
 }
 
-onSaveBtnClick = () => {
+window.onSaveBtnClick = function onSaveBtnClick() {
     t.set(context.card, 'shared', {requirementChangeCount})
         .then(() => {
                 showRequirementChangeCount(`Total Changes: ${requirementChangeCount}` + '(save successfully!)');
@@ -20,9 +77,41 @@ onSaveBtnClick = () => {
             () => {
                 showRequirementChangeCount(`Total Changes: ${requirementChangeCount}` + '(failed to save!)');
             });
+
+    t.card('id', 'desc').then(res => {
+        console.log('id', res);
+        info.cardId = res.id;
+        info.descriptions = res.desc;
+        info.version = `v${requirementChangeCount}.0`;
+        axios.post("http://localhost:8086/description", info).then(res => {
+
+            const btnList = document.getElementsByTagName("button");
+            let versionBtnList = [];
+            for (let i = 0; i < btnList.length; i++) {
+                if(btnList[i].textContent.substr(0, 1) === "v") {
+                    versionBtnList.push(btnList[i]);
+                }
+            }
+
+            if(versionBtnList.length < 5) {
+                axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+                    let versionRecord = document.getElementById("versionRecord");
+                    addBtnForVersionRecord(list, versionRecord);
+                });
+            }
+            else {
+                axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+
+                    for (let i = list.data.length - 1, j = 0; i >= list.data.length - 5, j < 5; i--, j++) {
+                        versionBtnList[j].textContent = list.data[i].version;
+                    }
+                });
+            }
+        });
+    });
 }
 
-showRequirementChangeCount = requirementChangeCount => {
+const showRequirementChangeCount = requirementChangeCount => {
     let element = document.getElementById('requirementChangeCount');
     element.innerHTML = requirementChangeCount;
 }
