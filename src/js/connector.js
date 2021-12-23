726db7b0fd6d4bf1d2dd79fca832db9b731c5542
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-23 17:14:55 +0800

    [yangyang] feat: extract connector method

--- src/js/connector.js
+++ src/js/connector.js
@@ -10,7 +10,7 @@ const onCardBtnClick = function (t) {
     });
 };
 
-const cardButtons = function () {
+const getCardButtons = function () {
     return [{
         text: 'Requirement Change',
         icon: 'https://uxwing.com/wp-content/themes/uxwing/download/19-e-commerce-currency-shopping/change-exchange.png',
@@ -19,35 +19,39 @@ const cardButtons = function () {
     }];
 }
 
+const getCardBadges = function (t) {
+        return t.get(t.getContext().card, 'shared', 'requirementChangeCount').then(res => {
+            if (res) {
+                return [{
+                    icon: 'https://uxwing.com/wp-content/themes/uxwing/download/19-e-commerce-currency-shopping/change-exchange.png',
+                    color: 'red'
+                }]
+            }
+        })
+}
+
+const getCardDetailBadges = function (t) {
+        return t.get(t.getContext().card, 'shared', 'requirementChangeCount')
+            .then(res => {
+                console.log('requirementChangeCount in res: ', res);
+                requirementChangeCount = res ? res : 0;
+                if (requirementChangeCount !== 0) {
+                    return [
+                        {
+                            title: "Changes",
+                            text: requirementChangeCount,
+                            color: 'red',
+                        },
+                    ];
+                }
+            })
+}
+
 window.TrelloPowerUp.initialize(
     {
         'board-buttons': getBoardButton,
-        'card-buttons': cardButtons,
-        'card-badges': function(t) {
-            return t.get(t.getContext().card, 'shared', 'requirementChangeCount').then(res => {
-                if(res) {
-                    return [{
-                        icon:'https://uxwing.com/wp-content/themes/uxwing/download/19-e-commerce-currency-shopping/change-exchange.png',
-                        color: 'red'
-                    }]
-                }
-            })
-        },
-        "card-detail-badges": function (t) {
-            return t.get(t.getContext().card, 'shared', 'requirementChangeCount')
-                .then(res => {
-                    console.log('requirementChangeCount in res: ', res);
-                    requirementChangeCount = res ? res : 0;
-                    if(requirementChangeCount !== 0) {
-                        return [
-                            {
-                                title: "Changes",
-                                text: requirementChangeCount,
-                                color: 'red',
-                            },
-                        ];
-                    }
-                })
-        },
+        'card-buttons': getCardButtons,
+        'card-badges': getCardBadges,
+        "card-detail-badges": getCardDetailBadges,
     }
 );
