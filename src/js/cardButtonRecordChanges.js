b6238cf656375ace16b118f693c50de454bfc3f8
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-24 10:56:38 +0800

    [yangyang] feat: #37 debug return values

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -56,11 +56,15 @@ const addBtnForVersionRecord = (list, versionRecord, curPage) => {
 }
 
 function onVersionBtnCLick(text) {
+    console.log("0.list: ", text);
     const savedDateTime = getSavedDateTime();
     axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+        console.log("1.list: ", list);
         const versionNum = parseInt(text.substring(1));
         const lastVersionNum = versionNum - 1;
         const lastVersionText = `v${lastVersionNum}.0`;
+        console.log("2.lastVersionNum: ", lastVersionNum);
+        console.log("3.lastVersionText: ", lastVersionText);
         let currentData;
         let oldData;
         list.data.forEach(item => {
@@ -71,13 +75,15 @@ function onVersionBtnCLick(text) {
                 oldData = item;
             }
         })
+        console.log("4.currentData: ", currentData);
+        console.log("5.oldData: ", oldData);
         const diff = Diff.diffChars(oldData.descriptions, currentData.descriptions);
+        console.log("6.versionDiff: ", diff);
         t.set(context.card, 'shared', {
             versionDiff: diff,
             savedTime: savedDateTime
-        }).then(() => console.log('set diff version'))
+        }).then(() => console.log('7.set diff version'))
     });
-    console.log('version page:', text);
     return t.modal({
         url: './versionComparisons.html',
         height: 500,
