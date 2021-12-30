8dd4a6e0a58069b67882250b5937645b62a4ef7d
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-12-30 16:22:42 +0800

    [yangyang] feat: #35 add change story card list in board button

--- src/js/getBoardButton.js
+++ src/js/getBoardButton.js
@@ -1,4 +1,31 @@
 export function getBoardButton(t, opts) {
+    let cardVersionRecordInfo = [];
+    t.cards('id', 'name', 'labels').then(cardList => {
+        cardList.forEach(card => {
+            let maxId = 0;
+            let lastTime = '';
+            let versionList = [];
+            axios.get(`http://localhost:8086/description/${card.id}`).then(function (res) {
+                // console.log("1.res: ", res);
+                if (res.data.length > 1) {
+                    res.data.forEach(version => {
+                        if (version.id > maxId) {
+                            maxId = version.id;
+                            lastTime = version.createdTime;
+                        }
+                        if (version.version !== "v0.0") {
+                            versionList.push(version.version);
+                        }
+                    })
+                    cardVersionRecordInfo = [...cardVersionRecordInfo, {...card, maxId, lastTime, versionList}];
+                    cardVersionRecordInfo = cardVersionRecordInfo.sort(function (a, b) {
+                        return a.maxId > b.maxId ? -1 : 1;
+                    })
+                    console.log("4.cardVersionRecordInfo: ", cardVersionRecordInfo);
+                }
+            })
+        })
+    })
     return [{
         icon: 'https://uxwing.com/wp-content/themes/uxwing/download/19-e-commerce-currency-shopping/change-exchange.png',
         text: 'Requirement Changes',
--- src/js/requirementChangesLists.js
+++ src/js/requirementChangesLists.js
@@ -1,3 +1,35 @@
-showLists = () => {
-    console.log('show changing lists there')
+import axios from "axios";
+const t = window.TrelloPowerUp.iframe();
+const Diff = require('diff');
+
+window.onVersionBoardBtnCLick = function onVersionBoardBtnCLick(text,cardId) {
+    axios.get(`http://localhost:8086/description/${cardId}`).then(list => {
+        const versionNum = parseInt(text.substring(1));
+        const lastVersionNum = versionNum - 1;
+        const lastVersionText = `v${lastVersionNum}.0`;
+        let currentData;
+        let oldData;
+        list.data.forEach(item => {
+            if (item.version === text) {
+                currentData = item;
+            }
+            if (item.version === lastVersionText) {
+                oldData = item;
+            }
+        });
+
+        const diff = Diff.diffChars(oldData.descriptions, currentData.descriptions);
+        let savedTime = currentData.createdTime;
+
+        return t.modal({
+            url: './versionComparisons.html',
+            args: {
+                text: diff,
+                savedTime: savedTime
+            },
+            height: 500,
+            fullscreen: false,
+            title: 'Description Comparison'
+        })
+    });
 }
