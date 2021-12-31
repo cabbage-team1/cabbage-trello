40a22bf9a0b5eae5115af474bd04c756ccb59d65
jcwangTw <jcwang@thoughtworks.com>
2021-12-31 11:15:24 +0800

    [jincheng] feat: try to fix http proxy middleware error.

--- src/js/setupProxy.js
+++ src/js/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        'http://localhost:8086/',
+        '/description',
         createProxyMiddleware(
             {
-                        target: 'http://122.51.213.254:8086/',
-                        changeOrigin: true
-                    }
+                target: 'http://122.51.213.254:8086',
+                changeOrigin: true
+            }
         )
     )
 }
